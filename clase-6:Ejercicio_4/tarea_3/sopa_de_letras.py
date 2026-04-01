f5320d303fd4973fe03985d225c064923a88f9a5
elemur19 <maria.murillom@ulead.ac.cr>
2026-03-31 18:07:54 -0600

    Updates - Order

--- clase-6:Ejercicio_4/tarea_3/sopa_de_letras.py
+++
@@ -1,7 +0,0 @@
-#Este es el que corre el script principal para mostrar al usuario del generador 
-
-from logica_sopa.generador import ejecutar_menu
-
-
-if __name__ == "__main__":
-    ejecutar_menu()
